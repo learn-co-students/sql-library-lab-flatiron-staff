296aaeaa08dabdc855cfdf267b530bfb0ffcd7c8
github username <anna.watt.lux@gmail.com>
2020-06-25 08:42:36 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,14 +11,13 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors LEFT OUTER JOIN series ON authors.id = series.author_id JOIN subgenres ON series.subgenre_id = subgenres.id;"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON authors.id = series.author_id JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  # series table for series.title
-  "SELECT series.title FROM series LEFT OUTER JOIN characters ON series.author_id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title LIMIT 1;"
+  "SELECT series.title FROM series JOIN characters ON series.author_id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) FROM characters LEFT OUTER JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC, characters.name;"
+  "SELECT characters.name, COUNT(character_books.book_id) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC, characters.name;"
 end
